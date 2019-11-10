4470c820fb71bd72a00c65341886c7959791e4fa
jsugijoto <jonathan.sugijoto@gmail.com>
2019-11-10 15:28:19 -0800

    BACKEND BOOM

--- backend/resources/genre.py
+++ backend/resources/genre.py
@@ -1,18 +1,27 @@
 # Genre Picker
 import json
+import os
 
 
 class SongPicker:
+    script_dir = os.path.dirname(__file__)
+
     @classmethod
     def loadName(cls, num, genre):
-        song = json.load(open("../../music/" + genre + "/songsLib.json"))
+        relative_path = "../../music/" + genre + "/songsLib.json"
+        abs_filepath = os.path.join(cls.script_dir, relative_path)
+        song = json.load(open(abs_filepath))
         songName = song[str(num)]
         return songName
+
     @classmethod
     def loadLyrics(cls, num, genre):
-        lyrics = json.load(open("../songs/", genre, "/lyricsLib.json"))
+        relative_path = "../../music/" + genre + "/lyricsLib.json"
+        abs_filepath = os.path.join(cls.script_dir, relative_path)
+        lyrics = json.load(open(abs_filepath))
         songLyrics = lyrics[str(num)]
         return songLyrics
+
     @classmethod
     def loadPath(cls, num, genre):
         return '../songs/' + genre + '/' + cls.loadName(num, genre) + '.mp3'
