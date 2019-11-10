023768d4dc600a928d79d46f2550473d65cf5798
Kevin James Hilton <kjhilton01@gmail.com>
2019-11-10 11:41:28 -0800

    get rid of linting errors

--- frontend/src/components/pages/Guess.js
+++ frontend/src/components/pages/Guess.js
@@ -1,9 +1,7 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import Countdown from "./../layout/Countdown";
 
 const Guess = props => {
-  const [genre, setGenre] = useState("");
-
   return (
     <Fragment>
       <h1 className="text-center text-primary">{props.genre}</h1>
