b93a8b675b01a8007d4bb6ba58b969f218856482
jsugijoto <jonathan.sugijoto@gmail.com>
2019-11-10 14:15:44 -0800

    leading slash

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from resources.game import GameCreator
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(GameCreator, '<string:genre>')
+api.add_resource(GameCreator, '/<string:genre>')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
