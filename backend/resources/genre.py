15bdf0b0694de9f04aab2dc2709ec2b865700ad2
jsugijoto <jonathan.sugijoto@gmail.com>
2019-11-10 11:50:10 -0800

    genreClass

--- backend/resources/genre.py
+++ backend/resources/genre.py
@@ -1,1 +1,22 @@
 # Genre Picker
+import ast
+
+
+class genrePicker():
+    parser = reqparse.RequestParser()
+    parser.add_argument('genre', type=str,
+                        required=True,
+                        help='Please pick a genre!'
+                        )
+
+    def pick():
+
+
+class pop():
+    d = json.load(open("popLib.json"))
+
+    def loadName(self, num):
+        song = d[num]
+
+    def loadLyrics(self, num):
+        lyrics = d[num]
