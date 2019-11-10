19db890751723a45ba03d3ce2430cb76595ffdd9
Kevin James Hilton <kjhilton01@gmail.com>
2019-11-10 12:47:37 -0800

    working with request

--- frontend/src/components/layout/Countdown.js
+++ frontend/src/components/layout/Countdown.js
@@ -1,20 +1,11 @@
 import React, { Fragment, useState } from "react";
 import countdown from "./countdown.gif";
-import axios from "axios";
 
 const Countdown = props => {
   const [visibility, setVisibility] = useState(true);
 
   setTimeout(() => {
     setVisibility(false);
-    axios
-      .get(`/song/${props.genre}`)
-      .then(res => {
-        console.log(res.song);
-      })
-      .catch(err => {
-        console.log("There was an error in requesting the song");
-      });
   }, 2700);
   let style = { width: "70%", margin: "auto", display: "block" };
   if (!visibility) style.display = "none";
