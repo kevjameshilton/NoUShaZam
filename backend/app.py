0e6d35300410b9651cedc499d97683c0779f43b2
Kevin James Hilton <kjhilton01@gmail.com>
2019-11-10 13:36:45 -0800

    virtual env

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Api
-from flask_pymongo import PyMongo
 
-from resources.genre import pick
+# from resources.genre import pick
 
 app = Flask(__name__)
 api = Api(app)
